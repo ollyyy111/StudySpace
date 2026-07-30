5c4742dcb1538c9b1a8cbe7fb3d1e4471f47600d
Olly <306521504+elliieee111@users.noreply.github.com>
2026-07-30 21:43:20 +0530

    Create StudySpace application window

---
+++ main.py
@@ -0,0 +1,12 @@
+import tkinter as tk
+import ttkbootstrap as ttk
+
+def main():
+    app = ttk.Window(themename="cosmo")
+    app.title("StudySpace")
+    app.geometry("900x600")
+
+    app.mainloop()
+
+if __name__ == "__main__":
+    main()   
