0669676b90812ed30b0b0bdbd5c725ae792b3939
Olly <306521504+elliieee111@users.noreply.github.com>
2026-08-01 16:06:30 +0530

    Completed step 13

--- main.py
+++ main.py
@@ -7,6 +7,11 @@ start_time = None
 elasped_time = 0
 app = None
 timer_label = None
+goals = []
+goal_entry = None
+goal_subject_entry = None
+goal_target_entry = None 
+goals_display = None 
 
 
 def load_subjects():
@@ -28,6 +33,105 @@ def save_subjects(subjects):
             indent=4
         )
 
+def load_goals():
+    try:
+        with open("goals.json","r") as file:
+            data=json.load(file)
+            return data.get("goals",[]) 
+    except FileNotFoundError:
+        return[]
+
+def save_goals(goals):
+    with open("goals.json","w") as file: 
+        json.dump(
+            {"goals": goals},
+            file,
+            indent=4
+        )             
+
+def delete_goal(goal):
+    global goals
+
+    if goal in goals:
+        goals.remove(goal)
+        save_goals(goals)
+        refresh_goals()
+
+def edit_goal(old_goal):
+    edit_window = tk.Toplevel()
+
+    edit_window.title("Edit Goal")
+    edit_window.geometry("300x250")
+    edit_window.resizable(False, False)
+
+    ttk.Label(
+        edit_window,
+        text="Goal Name"
+    ).pack(pady=5)
+
+    name_entry = ttk.Entry(edit_window, width=25)
+    name_entry.pack()
+
+    name_entry.insert(
+        0,
+        old_goal["goal"]
+    ) 
+
+    ttk.Label(
+        edit_window,
+        text="Subject"
+    ).pack(pady=5)
+
+    subject_entry_edit = ttk.Entry(
+        edit_window,
+        width=25
+    )
+    subject_entry_edit.pack()
+    subject_entry_edit.insert(
+        0,
+        old_goal["subject"]
+    )
+
+    ttk.Label(
+        edit_window,
+        text="Target Hours"
+    ).pack(pady=5)
+
+    target_entry = ttk.Entry(
+        edit_window,
+        width=25
+    )
+    target_entry.pack()
+    target_entry.insert(
+        0,
+        str(old_goal["target_hours"])
+    )
+
+    def save_edit():
+
+        new_name = name_entry.get().strip()
+        new_subject = subject_entry_edit.get().strip().title()
+        new_target = target_entry.get().strip()
+
+        if not new_name or not new_subject or not new_target:
+            return
+
+        old_goal["goal"] = new_name
+        old_goal["subject"] = new_subject
+        old_goal["target_hours"] = int(new_target)
+
+        save_goals(goals)
+        refresh_goals()
+
+        edit_window.destroy()
+
+    ttk.Button(
+        edit_window,
+        text="Save",
+        command=save_edit
+    ).pack(pady=15)    
+
+       
 
 def add_subject(subject_list, subject_name):
     subject_name = subject_name.strip().title()
@@ -238,19 +342,152 @@ def update_timer():
 
     app.after(1000, update_timer)
 
-                        
+def add_goal():
+    global goals
+
+    goal_name = goal_entry.get().strip()
+    subject = goal_subject_entry.get().strip().title()
+    target = goal_target_entry.get().strip()
+
+    if not goal_name or not subject or not target:
+        return
+    goal = {
+        "goal": goal_name,
+        "subject":subject,
+        "target_hours":int(target),
+        "completed_hours":0
+    }
+    goals.append(goal)
+    save_goals(goals)
+    refresh_goals()
+
+    goal_entry.delete(0, tk.END)
+    goal_subject_entry.delete(0, tk.END)
+    goal_target_entry.delete(0, tk.END)
+
+def refresh_goals():
+    for widget in goals_display.winfo_children():
+        widget.destroy()
+
+    for goal in goals:
+        create_goal_card(
+            goals_display,
+            goal
+        )    
+
+def create_goal_card(parent, goal):
+
+    card = ttk.Frame(
+        parent,
+        padding=10
+    )
+
+    card.pack(
+        fill="x",
+        padx=10,
+        pady=5
+    )
+
+    ttk.Label( 
+        card,
+        text=f"🎯 {goal['goal']}",
+        font=("Arial",13, "bold")
+    ).pack(anchor="w")
+
+    ttk.Label(
+        card,
+        text=f"Subject:{goal['subject']}"
+    ).pack(anchor="w")
+
+    completed = goal.get(
+        "completed_hours",
+        0
+    )
+
+    target = goal.get(
+        "target_hours",
+        1
+    )
+
+    percentage = int(
+        (completed / target) * 100
+    )
+
+    if percentage >= 100:
+        status = "🟢 Completed"
+    else:
+        status = "🟡 In Progress"    
+
+    ttk.Label(
+        card,
+        text=f"Progress: {percentage}%"
+    ).pack(anchor="w")
+
+    progress_bar = ttk.Progressbar(
+        card,
+        length=300,
+        value=percentage
+    )
+    progress_bar.pack(
+        anchor="w",
+        pady=5
+    )
+
+    ttk.Label(
+        card,
+        text=f"{completed} / {target} hours"
+    ).pack(anchor="w")
+
+    ttk.Label(
+        card,
+        text=status
+    ).pack(anchor="w")
+
+    button_frame = ttk.Frame(card)
+    button_frame.pack(
+        anchor="e",
+        pady=5
+    )
+
+    edit_button = ttk.Button(
+        button_frame,
+        text="✏️ Edit",
+        command=lambda: edit_goal(goal)
+    )
+
+    edit_button.pack(
+        side="left",
+        padx=5
+    )
+
+    delete_button = ttk.Button(
+        button_frame,
+        text="🗑️ Delete",
+        command=lambda: delete_goal(goal)
+    )
+    delete_button.pack(
+        side="left",
+        padx=5
+    )
+
 def main():
     global subjects
     global subject_entry
     global subjects_display
     global app
     global timer_label
-
+    global goals
+    global goal_entry
+    global goal_subject_entry
+    global goal_target_entry
+    global goals_display
+     
     app = ttk.Window(themename="cosmo")
     app.title("StudySpace")
-    app.geometry("950x850")
+    app.geometry("1000x1500")
 
     subjects = load_subjects()
+    goals = load_goals()
 
     title_label = ttk.Label(
         app,
@@ -277,6 +514,48 @@ def main():
         pady=10
     )
 
+    goals_frame = ttk.LabelFrame(
+        app,
+        text="Study Goals",
+        padding=20
+        )
+    goals_frame.pack(
+        fill="x",
+        padx=40,
+        pady=10
+    ) 
+
+    goal_entry = ttk.Entry(
+        goals_frame,
+        width=10
+    )
+    goal_entry.pack(pady=5)
+
+    goal_subject_entry = ttk.Entry(
+        goals_frame,
+        width=30
+    )
+    goal_subject_entry.pack(pady=5)
+
+    goal_target_entry = ttk.Entry(
+        goals_frame,
+        width=30
+    )
+    goal_target_entry.pack(pady=5)
+
+    add_goal_button = ttk.Button(
+        goals_frame, 
+        text="➕ Add Goal",
+        command=add_goal
+    )
+    add_goal_button.pack(pady=5)    
+
+    goals_display = ttk.Frame(goals_frame)
+    goals_display.pack(
+        fill="x",
+        pady=5
+    )
+    
     timer_label = ttk.Label(
         progress_frame,
         text="00:00:00",
@@ -326,7 +605,7 @@ def main():
         fill="both",
         expand=True,
         padx=40,
-        pady=10
+        pady=5
     )
 
     subject_entry = ttk.Entry(
@@ -343,7 +622,10 @@ def main():
     )
     add_subject_button.pack(pady=5)
 
-    canvas = tk.Canvas(subjects_frame)
+    canvas = tk.Canvas(
+        subjects_frame,
+        height=250
+    )
     scrollbar = tk.Scrollbar(
         subjects_frame,
         orient="vertical",
@@ -351,12 +633,16 @@ def main():
     )
 
     subjects_display = ttk.Frame(canvas)
+    def update_scroll(e):
+        canvas.configure(
+            scrollregion=canvas.bbox("all")
+        )
     subjects_display.bind(
         "<Configure>",
-        lambda e: canvas.configure(
-            scrollregion=canvas.bbox("all")
-        )    
-    )
+        update_scroll
+            
+        )   
+
     canvas_frame = canvas.create_window(
         (0,0),
         window=subjects_display,
@@ -391,6 +677,7 @@ def main():
     refresh_subjects()
 
     update_timer()
+    refresh_goals()
     app.mainloop()
 
 
